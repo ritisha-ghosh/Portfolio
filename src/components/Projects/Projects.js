ca1b2368edfbc6c7449b72847b71202e48b3d7f5
Ritisha Ghosh <ritisha.social@hotmail.com>
2025-02-13 13:46:48 +0530

    Updated Resume page with Coming Soon message and gradient background

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import pic1 from "../../Assets/Projects/portfolio.png";
+import pic2 from "../../Assets/Projects/protective.png";
 
 
 
@@ -22,12 +23,23 @@ function Projects() {
               isBlog={false}
               title="Portfolio Website"
               description="This is my personal Portfolio Website which I have created using React.js. I have shared about myself, my skills, projects, and archievements."
-              ghLink=" " // Update with your GitHub link
+              ghLink="" // Update with your GitHub link
               demoLink=" " // Update with your demo link
             />
           </Col>
 
           
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={pic2} // Update this line to use your portfolio image
+              isBlog={false}
+              title="Protective : Empowerment Of Women"
+              description="This is a group project with my college friends. We started it for Smart India Hackathon 2024. It is a web application which is used to provide the safety for women. We are still working on this. My teammates are Pritam Chakraborty, Arghya Jana, Shreya Sarkar, Swagata Roychowdhury, Souvik Dutta and our mentor Dr. Shanta phani (our HOD, CSE Department)."
+              ghLink="https://github.com/ritisha-ghosh/Protective-SIH" // Update with your GitHub link
+              demoLink="https://protective.vercel.app" // Update with your demo link
+            />
+          </Col>
+
         </Row>
       </Container>
     </Container>
